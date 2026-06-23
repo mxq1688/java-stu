e4b068807351d1667bed5bbdc42986cbd84e4259
xianqiang.meng <xianqiang.meng@mobvoi.com>
2026-06-23 18:37:20 +0800

    update

---
+++ my-java-maven-app/src/main/java/com/example/my_java_maven_app/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.my_java_maven_app.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    pri
+
+}
